79f364d757961e9da32913f5a96071fcc350584e
mihad-khadem <mihadkhadem@gmail.com>
2023-07-29 15:27:40 +0600

    Simple problem solveing with javascript

---
+++ problem1.js
@@ -0,0 +1,13 @@
+// Harry mother gave him 1000tk
+// To buy 1kg Apples & 1 Kg Orange
+
+
+var motherGave = 1000;
+
+var applePrice = 250;
+var orangePrice =360;
+
+var sum = applePrice + orangePrice;
+
+console.log(sum);
+console.log(motherGave - sum);
---
+++ problem2.js
@@ -0,0 +1,12 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var sumOfMarks = math + biology + chemistry + physics + bangla ;
+
+var numOfSub = 5;
+var avgMark = sumOfMarks / numOfSub ; 
+avgMark = avgMark.toFixed(2);
+console.log("The average mark is =" + avgMark);
